a996639837974898c87e36fc53a015d8bc160a59
Leon-Jerke <62246987+Leon-Jerke@users.noreply.github.com>
2022-05-02 23:41:23 +0200

    Added first FrontEnt

---
+++ ContactSplitter/Frontend/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ContactSplitter.Frontend.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactSplitter.Frontend.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand Page2ViewCommand { get; set; }
+
+        public NameSplitterViewModel NameSplitterVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            NameSplitterVM = new NameSplitterViewModel();
+
+            CurrentView = NameSplitterVM;
+
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = NameSplitterVM;
+            });
+
+
+        }
+
+    }
+}
---
+++ ContactSplitter/Frontend/ViewModel/NameSplitterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ContactSplitter.Frontend.Core;
+using System.Windows.Input;
+
+namespace ContactSplitter.Frontend.ViewModel
+{
+    class NameSplitterViewModel : ObservableObject
+    {
+        private string _textInputString;
+        public string TextInputString
+        {
+            get { return _textInputString; }
+            set { Update(ref _textInputString, value); }
+        }
+
+        private string _textOutputString;
+        public string TextOutputString
+        {
+            get { return _textOutputString; }
+            set { Update(ref _textOutputString, value); }
+        }
+
+        public ICommand SubmitTextCommand => new RelayCommand(x => this.OnSplitButtonClicked());
+
+
+        private void OnSplitButtonClicked()
+        {
+            TextOutputString = this.TextInputString;
+        }
+    }
+}
