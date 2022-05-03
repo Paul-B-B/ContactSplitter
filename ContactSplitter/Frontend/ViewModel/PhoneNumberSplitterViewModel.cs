65ce728eb38c00723e06ca09b47867df7dfa0dcc
Leon-Jerke <62246987+Leon-Jerke@users.noreply.github.com>
2022-05-03 11:07:53 +0200

    smaller visual changes

---
+++ ContactSplitter/Frontend/ViewModel/PhoneNumberSplitterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactSplitter.Frontend.ViewModel
+{
+    internal class PhoneNumberSplitterViewModel
+    {
+    }
+}
