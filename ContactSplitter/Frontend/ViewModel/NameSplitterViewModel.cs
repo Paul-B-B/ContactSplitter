b0075a2540e6001fd8e2f4fc5582614a4cc928d7
Leon-Jerke <62246987+Leon-Jerke@users.noreply.github.com>
2022-05-04 13:19:28 +0200

    NameSplitter Layout

--- ContactSplitter/Frontend/ViewModel/NameSplitterViewModel.cs
+++ ContactSplitter/Frontend/ViewModel/NameSplitterViewModel.cs
@@ -1,15 +1,23 @@
 ﻿using ContactSplitter.Frontend.Core;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ContactSplitter.Frontend.ViewModel
 {
     class NameSplitterViewModel : ObservableObject
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private string _textInputString;
         public string TextInputString
         {
             get { return _textInputString; }
-            set { Update(ref _textInputString, value); }
+            set 
+            { 
+                Update(ref _textInputString, value);
+                this.OnTextInputChanged();
+            }
         }
 
         private string _textOutputString;
@@ -21,10 +29,16 @@ namespace ContactSplitter.Frontend.ViewModel
 
         public ICommand SubmitTextCommand => new RelayCommand(x => this.OnSplitButtonClicked());
 
-
         private void OnSplitButtonClicked()
         {
             TextOutputString = this.TextInputString;
         }
+
+        private void OnTextInputChanged()
+        {
+            TextOutputString = this.TextInputString;
+        }
+
+
     }
 }
